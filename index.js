bcfec51d910c4437ae7ee125893e08bf23a93558
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-07-26 21:45:00 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1=Number("31");
+let num2=Number("2");
+let multiply=num1*num2;
+console.log(multiply);
 
-// Write your code here
+function random(){
+    num1=Math.random();
+    console.log(typeof(num1));
+    return num1;
+}
+console.log(random());
+let highest=(a,b,c,d,e)=>{
+    max = Math.max(a,b,c,d,e);
+    return max;
+}
+let num3=Number("5");
+let num4=Number("24");
+let mod=x2%x1;
+console.log(mod);
